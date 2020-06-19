80e054d84f60e98552ff45fed6ee6282e66c8549
Sławomir Lasik <--global>
2020-06-19 13:56:24 +0200

    Created Employee DAO Implementation for Hibernate

---
+++ src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,14 @@
+package com.slawomirlasik.cruddemo.dao;
+
+import com.slawomirlasik.cruddemo.entity.Employee;
+
+import java.util.List;
+
+
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+}
