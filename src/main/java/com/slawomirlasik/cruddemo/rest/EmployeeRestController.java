62b8a5a5a4b6841627451ed2c64293ec152437a0
Sławomir Lasik <--global>
2020-06-19 14:21:35 +0200

    Injected EmployeeDAO and created mapping for getting list of employees

--- src/main/java/com/slawomirlasik/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/slawomirlasik/cruddemo/rest/EmployeeRestController.java
@@ -1,13 +1,29 @@
 package com.slawomirlasik.cruddemo.rest;
 
+import com.slawomirlasik.cruddemo.dao.EmployeeDAO;
+import com.slawomirlasik.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
+    private EmployeeDAO employeeDAO;
+
     // quick and dirty: inject employee dao
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
 
     // expose "/employees"  endpoint to return all employees
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
 }
