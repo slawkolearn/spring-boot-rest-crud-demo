9679f2ec74abdd8ca43614f46a22cc3f2e40142a
Sławomir Lasik <--global>
2020-06-19 13:57:02 +0200

    Anootated EmployeeDAOHibernateImpl  with @Repository

--- src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -1,10 +1,11 @@
 package com.slawomirlasik.cruddemo.dao;
 
 import com.slawomirlasik.cruddemo.entity.Employee;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
     @Override
