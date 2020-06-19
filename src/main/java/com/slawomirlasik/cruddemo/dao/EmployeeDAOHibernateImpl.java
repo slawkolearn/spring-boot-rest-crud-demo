fe986632c74fe4d92f7b3d7eb06dde5108f6101c
Sławomir Lasik <--global>
2020-06-19 14:10:00 +0200

    ADded method findAll in EmployeeDAOHibernateImpl @Transactional for automatic commiting and opening sessions

--- src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -1,15 +1,39 @@
 package com.slawomirlasik.cruddemo.dao;
 
 import com.slawomirlasik.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
+    // define field for entitymanager
+
+    private EntityManager entityManager;
+
+    // setup constructor injection
+
+    @Autowired
+    public EmployeeDAOHibernateImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
+    @Transactional
     public List<Employee> findAll() {
+
+        // get the current hibernate session
+
+
+        // create a query
+
+        // execute the query
+
+        // return the results
         return null;
     }
 }
