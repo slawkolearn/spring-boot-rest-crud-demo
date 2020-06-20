b2ddf9bf7b726fc3e14f35039d1ad870cc319462
Sławomir Lasik <--global>
2020-06-20 15:20:34 +0200

    Added EmployeeServiceImpl with stubbed methods

---
+++ src/main/java/com/slawomirlasik/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.slawomirlasik.cruddemo.service;
+
+import com.slawomirlasik.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
