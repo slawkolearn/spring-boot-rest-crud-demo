c9d0b16eaf6ecc48ef50d0aa8c16cca26cb69d20
Sławomir Lasik <--global>
2020-06-20 15:51:52 +0200

    Added in EmployeeRestController method for getting signle employee by ID

--- src/main/java/com/slawomirlasik/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/slawomirlasik/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.slawomirlasik.cruddemo.entity.Employee;
 import com.slawomirlasik.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,22 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
+
+
+    // add mapping for GET /employee/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if( (theEmployee == null)) {
+            throw new RuntimeException("Employee id not found " + employeeId);
+
+        }
+
+        return theEmployee;
+
+    }
+
 }
