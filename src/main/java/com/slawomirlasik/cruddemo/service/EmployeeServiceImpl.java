6d2f73de2f1c77cb8937c584d77c4d5564647367
Sławomir Lasik <--global>
2020-06-20 15:22:07 +0200

    Injected the EmployeeDAO in EmployeeServiceImpl

--- src/main/java/com/slawomirlasik/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/slawomirlasik/cruddemo/service/EmployeeServiceImpl.java
@@ -1,10 +1,24 @@
 package com.slawomirlasik.cruddemo.service;
 
+import com.slawomirlasik.cruddemo.dao.EmployeeDAO;
 import com.slawomirlasik.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
+        employeeDAO = theEmployeeDAO;
+    }
+
+
+
     @Override
     public List<Employee> findAll() {
         return null;
