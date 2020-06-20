b8527a272d342f688ee27a3ab0c3086ebae87843
Sławomir Lasik <--global>
2020-06-20 15:06:14 +0200

    Added additional method for EmployeeDAO to implement

--- src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/slawomirlasik/cruddemo/dao/EmployeeDAO.java
@@ -8,4 +8,10 @@ public interface EmployeeDAO {
 
     public List<Employee> findAll();
 
+    public Employee findById(int id);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int id);
+
 }
