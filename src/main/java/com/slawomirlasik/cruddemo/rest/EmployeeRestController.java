ab5f321768a2630a1d97b29a8d6fcea373ce2316
Sławomir Lasik <--global>
2020-06-20 16:00:07 +0200

    Added method in EmployeeRestController for saving employee send with JSON format

--- src/main/java/com/slawomirlasik/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/slawomirlasik/cruddemo/rest/EmployeeRestController.java
@@ -4,10 +4,7 @@ import com.slawomirlasik.cruddemo.dao.EmployeeDAO;
 import com.slawomirlasik.cruddemo.entity.Employee;
 import com.slawomirlasik.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +41,21 @@ public class EmployeeRestController {
 
         return theEmployee;
 
+    }
+
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody // Binding JSON to employee object
+                                            Employee theEmployee){
+
+        // also just in case they pass an id in JSON .. set id to 0
+        // this is to force a save of new item .. instead of update
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
     }
 
 }
